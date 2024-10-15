8160f35b7d6ba384ca59295943fd970971c7c5af
Lungu Mihai Adrian <adrian.lungu@csie.ase.ro>
2024-10-15 15:31:27 +0300

    for each example

---
+++ obj.js
@@ -0,0 +1,25 @@
+const obj = {
+    name: 'Adrian',
+    meet: function () {
+        console.log(this.name)
+    },
+    meet2: () => {
+        console.log(this.name)
+    }
+}
+
+// console.log(obj)
+
+obj.yearsOld = 25;
+
+// console.log(obj)
+
+// obj = {}
+
+const arr = [1,2,3,4]
+
+arr.push(5);
+
+// arr = [6,7,8]
+
+obj.meet2();
