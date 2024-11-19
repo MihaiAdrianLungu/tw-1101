5a3a87b4b4902ee11e6ede8e6288c1d048ca4d09
Lungu Mihai Adrian <adrian.lungu@csie.ase.ro>
2024-11-19 15:52:13 +0200

    seminar 8

---
+++ seminar8/server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require("dotenv");
+const userRoutes = require('./routes/user.routes');
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(cors({
+    origin: 'http://127.0.0.1:5500'
+}));
+app.use('/users', userRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://127.0.0.1:${PORT}`)
+});
---
+++ seminar8/server/routes/user.routes.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const User = require('../database/models/User');
+const router = express.Router();
+
+module.exports = router;
